1fb229d343a209b13eb484bb0ff3b1029605ae61
Juan Sebastian Rincon <juarinconpi@unal.edu.co>
2026-09-25 00:56:33 -0500

    Funciones de movimiento

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,37 @@
 package src;
 import java.util.Random;
 public class Main {
-    public static void main(String[] args) {
-        Random aleatorio = new Random();
-        Snake snake = new Snake(2);
-        snake.addHead(new Position(0, 0)); 
-        snake.addHead(new Position(0, 1)); 
 
-        Position newHead = new Position(0, 2);
+    public static void moverse(int fila, int Columna, Snake snake){ //Se mueve sin haber comido
+        Position newHead = new Position(fila, Columna);
+        Boolean colision = false;
+        for (int i = 0; i<snake.size();i++){
+            if (snake.get(i).equals(newHead)){
+                System.out.print("Game Over");
+                colision = true;
+            }
+        }
+        if (!colision){
+            snake.addHead(newHead);
+            snake.removeTail();
+        }
+
+        for (int i = 0; i<snake.size();i++){
+            Position p = snake.get(i);
+            if (i==0){
+                System.out.println("Cola   "+i+": fila = " + p.row() + ": Columna = "+ p.column());
+            } else if (i==snake.size()-1) {
+                System.out.println("Cabeza "+i+": fila = " + p.row() + ": Columna = "+ p.column());
+            } else {
+                System.out.println("Cuerpo "+i+": fila = " + p.row() + ": Columna = "+ p.column());
+            }
+
+        }
+    }
+
+    public static void avecesDaAVecesQuita(int fila, int Columna, Snake snake){ //A veces come a veces no
+        Random aleatorio = new Random();
+        Position newHead = new Position(fila, Columna);
         Boolean colision = false;
         for (int i = 0; i<snake.size();i++){
             if (snake.get(i).equals(newHead)){
@@ -27,14 +51,32 @@ public class Main {
         for (int i = 0; i<snake.size();i++){
             Position p = snake.get(i);
             if (i==0){
-                System.out.println("Cola "+i+": fila =" + p.row() + ": Columna = "+ p.column());
+                System.out.println("Cola   "+i+": fila = " + p.row() + ": Columna = "+ p.column());
             } else if (i==snake.size()-1) {
-                System.out.println("Cabeza "+i+": fila =" + p.row() + ": Columna = "+ p.column());
+                System.out.println("Cabeza "+i+": fila = " + p.row() + ": Columna = "+ p.column());
             } else {
-                System.out.println("Cuerpo "+i+": fila =" + p.row() + ": Columna = "+ p.column());
+                System.out.println("Cuerpo "+i+": fila = " + p.row() + ": Columna = "+ p.column());
             }
 
         }
-
     }
-}
+    public static void main(String[] args) {
+        Snake snake = new Snake(2);
+        snake.addHead(new Position(0, 0)); 
+        snake.addHead(new Position(0, 1)); 
+
+        moverse(0, 2, snake);
+        System.out.println();
+        moverse(1, 2, snake);
+        System.out.println();
+        moverse(2, 2, snake);
+        System.out.println();
+        avecesDaAVecesQuita(3, 2, snake);
+        System.out.println();
+        avecesDaAVecesQuita(4, 2, snake);
+        System.out.println();
+        avecesDaAVecesQuita(4, 3, snake);
+        System.out.println();
+        moverse(4, 2, snake);
+    }
+}   
